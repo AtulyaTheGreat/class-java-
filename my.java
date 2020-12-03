b424e23510eafb3a13608ee2ac6ecb4c11ce8f94
AtulyaTheGreat <75066157+AtulyaTheGreat@users.noreply.github.com>
2020-12-03 13:35:33 +0530

    Create my.java

---
+++ my.java
@@ -0,0 +1,16 @@
+public class My {
+    
+    public void method(String name) {
+        System.out.println("Hello " + name);
+    }
+
+    public static void main(String[] args) {
+        My obj = new My();
+
+        obj.method("Atulya");
+        
+
+
+        
+    }
+}
